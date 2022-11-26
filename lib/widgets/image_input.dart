c9d303e03ef0bd931c23c7c3fe71872c64773e19
Sayed31 <='sayed.sishoieb@gmail.com>
2022-11-26 17:37:18 +0200

    Storing the Image on the Filesystem (on the Device)

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
+
 //This is a great plug in which makes it very easy to work with the device camera,
 // select the image or
 // take an image and then work with that image.
+import 'package:path_provider/path_provider.dart' as syspaths;
+// this is a package which helps you get access to the paths
+// iOS and Android give you to store data in.
+import 'package:path/path.dart' as path;
+// The path package provides common operations for manipulating paths: joining, splitting,
+// normalizing, etc. so basically it helps with constructing the path that found n gained to
+//acces by the path provider. This package helps us with combining paths basically.
 
 class imageInput extends StatefulWidget {
   const imageInput({Key? key}) : super(key: key);
@@ -13,7 +21,7 @@ class imageInput extends StatefulWidget {
 }
 
 class _imageInputState extends State<imageInput> {
-  // late File _storedImage;
+  File? _storedImage;
 
   Future<void> _takePicture() async {
     final picker = ImagePicker();
@@ -21,6 +29,25 @@ class _imageInputState extends State<imageInput> {
       source: ImageSource.camera,
       maxWidth: 600,
     );
+    if (imageFile != null) {
+      setState(() {
+        _storedImage = File(imageFile!
+            .path); // what u get from pickimage is not a file it a picked file
+        // so to convert it to file we use File constructor and pss to it the path of the imageFile
+      });
+      final appDir = await syspaths.getApplicationDocumentsDirectory();
+      final fileName = path.basename(imageFile.path);
+      //gives you The part of a path after the last separator,
+      // which is a complex name for saying the file name.
+
+      final savedImage = await _storedImage!
+          .copy('${appDir}/${fileName}'); // copying the image to a path
+      // in the device hard for storing it there
+      // we could just typed await _storedImage!
+      //           .copy(appDir);
+      // but you also have to provide the name of the image it should have.
+      // n i wanted to store it with the image name automatically assigned to it
+    }
   }
 
   @override
@@ -36,18 +63,16 @@ class _imageInputState extends State<imageInput> {
               color: Colors.grey,
             ),
           ),
-          child:
-              // _storedImage != null
-              //     ? Image.file(
-              //   _storedImage,
-              //   fit: BoxFit.cover,
-              //   width: double.infinity,
-              // )
-              //     :
-              Text(
-            'No Image Taken',
-            textAlign: TextAlign.center,
-          ),
+          child: _storedImage != null
+              ? Image.file(
+                  _storedImage!,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                )
+              : Text(
+                  'No Image Taken',
+                  textAlign: TextAlign.center,
+                ),
           alignment: Alignment.center,
         ),
         SizedBox(
